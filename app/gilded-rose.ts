3f0cc53b42e10a463aebd13ebf7f8d4da38e2253
Vlad Muntean <Vlad.Muntean@softwire.com>
2025-07-04 13:49:46 +0300

    Optimize name checking

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,14 +21,14 @@ export class GildedRose {
         for (let i = 0; i < this.items.length; i++) {
 
             // Skip for sulfuras
-            if (this.items[i].name == 'Sulfuras, Hand of Ragnaros')
+            if (this.items[i].name.includes('Sulfuras'))
                 continue;
 
             // Modify sellIn
             this.items[i].sellIn = this.items[i].sellIn - 1;
 
             // Modify quality
-            if (!['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'].includes(this.items[i].name)) {
+            if(!(this.items[i].name.includes('Brie') || this.items[i].name.includes('Backstage pass'))) {
 
                 // Check if is sold in time
                 let decreaseQuality = this.items[i].sellIn < 0 ? 2 : 1;
@@ -36,7 +36,7 @@ export class GildedRose {
 
             } else {
                 let addToQuality = this.items[i].sellIn < 0 ? 2 : 1;
-                if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                if (this.items[i].name.includes('Backstage pass')) {
                     if (this.items[i].sellIn < 11) {
                         addToQuality = addToQuality + 1
                     }
